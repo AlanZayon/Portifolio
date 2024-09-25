87ffff6486830b72e4a6b76a5bdac72e753c4df3
Alan Zayon <alanzayon72@gmail.com>
2024-09-25 14:47:11 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+function sendEmail() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+  
+    if (!firstName || !lastName || !subject || !message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+  
+    const emailBody = `Name: ${firstName} ${lastName}\n\nMessage:\n${message}`;
+  
+    window.location.href = `mailto:alanzayon82@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(emailBody)}`;
+  }
+  
